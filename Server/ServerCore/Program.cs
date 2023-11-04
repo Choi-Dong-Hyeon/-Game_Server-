4b9472bf8005c34a77e39309aa93907b81fc9071
Choi <vwwwwvww@gmail.com>
2023-11-05 02:03:25 +0900

    commit

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //ThreadPool.SetMinThreads(1, 1);
+            //ThreadPool.SetMaxThreads(5, 5);
+
+            for (int i = 0; i < 8; i++)
+                ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
+
+           
+
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            //for(int i=0; i < 10000; i++)
+            //{
+
+            //Thread t = new Thread(MainThread);
+            ////t.Name = "TestThread";
+            //t.IsBackground = true;
+            //t.Start();
+            //}
+
+            //Console.WriteLine("Waiting Hello, World!");
+
+            //t.Join();
+
+            //Console.WriteLine("Hello, World!");
+
+
+            while (true) { }
         }
+
+        static void MainThread(object? state)
+        {
+            int count = 0;
+            while (true)
+            {
+                Console.WriteLine(count++);
+            }
+        }
+
     }
+
+
+
+
+
 }
